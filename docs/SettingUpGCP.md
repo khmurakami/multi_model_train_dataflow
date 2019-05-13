e08a8cafd8eb05dc7b098824b48ab4940b1da30b
unknown <kalanimurakami1218@gmail.com>
2019-05-13 10:00:36 -0700

    First draft of pipeline

---
+++ docs/SettingUpGCP.md
@@ -0,0 +1,9 @@
+# How to Setup GCP on the Console
+
+# Creating an Account
+
+# Google Big Query
+
+# Google Dataflow
+
+# Google Bucket
--- multi_model_train/multi_model_train.py
+++ multi_model_train/multi_model_train.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# coding=utf-8
 
+# This file handles the parallelization of training
+
+from __future__ import unicode_literals
 
 # System Libraries
 import itertools
@@ -12,64 +15,36 @@ import apache_beam as beam
 import tensorflow as tf
 
 # Third Party imports
-from sklearn import cross_validation
+from sklearn.model_selection import cross_validate
+from apache_beam.io import WriteToText
 
-
-def dict_product(param_grid):
-
-    """Create all the difference configurations of params a model can be trained in
-
-    Args:
-        param_grid (list): A list of all the possible params
-
-    Return:
-
-
-    """
-
-    param_permutations = (dict(itertools.izip(param, x)) for x in itertools.product(*param.itervalues()))
-
-    return param_permutations
-
-
-def all_parameters():
-
-    """All the parameters you want to permuatate
-
-    """
-
-    # Create a Dictionary
-    param_grid = {'hidden_units': [[10, 20, 10], [20, 40, 20], [100, 200, 100]],
-                  'dropout': [0.1, 0.2, 0.5, 0.8],
-                  'steps': [20000, 50000, 100000]}
-
-    params = list(dict_product(param_grid))
-
-    return params
+import random
+import string
 
 
 class MultiModelTrain():
 
     def __init__(self):
 
-        self.options = beam.utils.pipeline_options.PipelineOptions()
+        self.options = beam.options.pipeline_options.PipelineOptions()
 
         # Google Cloud possible options
-        self.google_cloud_options = self.options.view_as(beam.utils.pipeline_options.GoogleCloudOptions)
-        self.google_cloud_options.project = '{PROJECTID}'
-        self.google_cloud_options.job_name = 'tensorflow-gs'
-        self.google_cloud_options.staging_location = 'gs://{BUCKET_NAME}/binaries'
-        self.google_cloud_options.temp_location = 'gs://{BUCKET_NAME}/temp'
-
+        self.google_cloud_options = self.options.view_as(beam.options.pipeline_options.GoogleCloudOptions)
+        self.google_cloud_options.project = 'multi-model-dataflow'
+        self.google_cloud_options.job_name = 'multi-mode-dataflow'
+        #self.google_cloud_options.staging_location = 'gs://{BUCKET_NAME}/binaries'
+        #self.google_cloud_options.temp_location = 'gs://{BUCKET_NAME}/temp'
+        self.google_cloud_options.staging_location = 'gs://multi_model_bucket/binaries'
+        self.google_cloud_options.temp_location = 'gs://multi_model_bucket/temp'
         # Apache Beam Worker Optiosn
-        self.worker_options = self.options.view_as(beam.utils.pipeline_options.WorkerOptions)
+        self.worker_options = self.options.view_as(beam.options.pipeline_options.WorkerOptions)
         self.worker_options.max_num_workers = 6
         self.worker_options.num_workers = 6
-        self.worker_options.disk_size_gb = 20
+        self.worker_options.disk_size_gb = 100
         # worker_options.machine_type = 'n1-standard-16'
 
         # options.view_as(beam.utils.pipeline_options.StandardOptions).runner = 'DirectRunner'
-        self.options.view_as(beam.utils.pipeline_options.StandardOptions).runner = 'DataflowRunner'
+        self.options.view_as(beam.options.pipeline_options.StandardOptions).runner = 'DataflowRunner'
 
         self.p = beam.Pipeline(options=self.options)
 
@@ -81,21 +56,30 @@ class MultiModelTrain():
 
         (self.p | 'init' >> beam.Create(params)
            | 'train' >> beam.Map(self.set_train)
-           | 'output' >> beam.Write(beam.io.BigQuerySink('project:dataset.table',
-                                    schema="accuracy:FLOAT, loss:FLOAT, model_id:STRING, param:STRING",
-                                    write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
-                                    create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED))
+           | 'output' >> beam.io.WriteToBigQuery(
+                                    "project:dataset.table",
+                                    schema='accuracy:FLOAT, loss:FLOAT, model_id:STRING, param:STRING',
+                                    write_disposition='WRITE_APPEND',
+                                    create_disposition='CREATE_IF_NEEDED')
          )
 
     def set_train(self, param):
 
         """Set the training to be used in Beam
 
+        Args:
+
+        Return:
+
         """
 
-        model_id = str(uuid.uuid4())
+        import tensorflow as tf
+        from sklearn.model_selection import cross_validation
+
+        model_id = self.randomString()
 
         # Load iris dataset
+        print("Loading Iris Data")
         iris = tf.contrib.learn.datasets.base.load_iris()
         train_x, test_x, train_y, test_y = cross_validation.train_test_split(
             iris.data, iris.target, test_size=0.2, random_state=0
@@ -104,20 +88,25 @@ class MultiModelTrain():
         # https://www.tensorflow.org/get_started/tflearn
         feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
         classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
-                                                    hidden_units=param['hidden_units'],
-                                                    dropout=param['dropout'],
+                                                    hidden_units=param["hidden_units"],
+                                                    dropout=param["dropout"],
                                                     n_classes=3,
-                                                    model_dir='gs://{BUCKET_NAME}/models/%s'% model_id)
+                                                    model_dir="gs://multi_model_bucket/models/%s"% model_id)
+
+        print("Training Model Now")
         classifier.fit(x=train_x,
                        y=train_y,
-                       steps=param['steps'],
+                       steps=param["steps"],
                        batch_size=50)
+
         result = classifier.evaluate(x=test_x, y=test_y)
 
-        ret = {'accuracy': float(result['accuracy']),
-               'loss': float(result['loss']),
-               'model_id': model_id,
-               'param': json.dumps(param)}
+        ret = {
+               "accuracy": float(result["accuracy"]),
+               "loss": float(result["loss"]),
+               "model_id": str(model_id),
+               "param": json.dumps(param)
+               }
 
         return ret
 
@@ -127,12 +116,61 @@ class MultiModelTrain():
 
         """
 
+        print("Sending the Process to Cloud")
         self.p.run()
+
+    def randomString(self, stringLength=10):
+
+        """Generate a random string of fixed length
+
+         """
+
+        import string
+        import random
+
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(stringLength))
+
+
+    def dict_product(self, param_grid):
+
+        """Create all the difference configurations of params a model can be trained in
+
+        Args:
+            param_grid (list): A list of all the possible params
+
+        Return:
+            param_permutations (dict): A dictionary of all the possible permutations
+
+
+        """
+
+        param_permutations = (dict(zip(param_grid, x)) for x in itertools.product(*param_grid.values()))
+
+        return param_permutations
+
+
+    def all_parameters(self):
+
+        """All the parameters you want to permuatate
+
+        """
+
+        # Create a Dictionary
+        param_grid = {
+                      'hidden_units': [[10, 20, 10], [20, 40, 20], [100, 200, 100]],
+                      'dropout': [0.1, 0.2, 0.5, 0.8],
+                      'steps': [20000, 50000, 100000]
+                     }
+
+        params = list(self.dict_product(param_grid))
+
+        return params
 
 
 if __name__ == "__main__":
 
     test = MultiModelTrain()
-    params = all_parameters()
+    params = test.all_parameters()
     test.set_parameters(params)
     test.run_training()
